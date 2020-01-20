8216bafd6e99a0004887b02e19ec2eb8e6a9bd2d
JAFFAR Tawfiq <tawfiq.jaffar@epitech.eu>
2020-01-20 17:17:34 +0100

    add android studio project

---
+++ source/app/src/main/java/com/tawfiqjaffar/workshopandroidpart1/Activities/Splash.kt
@@ -0,0 +1,13 @@
+package com.tawfiqjaffar.workshopandroidpart1.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tawfiqjaffar.workshopandroidpart1.R
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
